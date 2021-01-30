87772786dee0642a5d62a7f553987467325523ee
trnhi1010 <57781716+trnhi1010@users.noreply.github.com>
2021-01-30 18:54:43 +0700

    [Nhi]: Update
    Move script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,27 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private float v;
+    private float h;
+    private Vector3 VerticalMovement;
+    private Vector3 HorizontalMovement;
+    private float speed = 10.0f;
+    private void FixedUpdate()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //get movement input from user
+        v = Input.GetAxisRaw("Vertical");
+        h = Input.GetAxisRaw("Horizontal");
+        //make movement input become vector3
+        VerticalMovement = new Vector3(0, 0, v);
+        HorizontalMovement = new Vector3(h, 0, 0);
+        //change local direction into world direction
+        VerticalMovement = transform.TransformDirection(VerticalMovement);
+        HorizontalMovement = transform.TransformDirection(HorizontalMovement);
+        //add speed into movement
+        VerticalMovement *= speed;
+        HorizontalMovement *= speed;
+        //add movement into position
+        transform.localPosition += VerticalMovement * Time.fixedDeltaTime;
+        transform.localPosition += HorizontalMovement * Time.fixedDeltaTime;
     }
 }
