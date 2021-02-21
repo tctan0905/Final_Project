0f60c33e74ec217e620705ffb451e6a56d0c2533
trnhi1010 <57781716+trnhi1010@users.noreply.github.com>
2021-02-21 18:51:50 +0700

    [Nhi]:Update UI
    meunu , option

--- Assets/Scripts/BombController.cs
+++ Assets/Scripts/BombController.cs
@@ -8,17 +8,30 @@ public class BombController : MonoBehaviour
     public float speedBullet;
     public float timeLife;
     public Rigidbody test;
+
+    public GameObject boomb;
+    public float bombDistance = 2f;
+    public float boombThrowingForce = 5f;
+
+    private bool holdingBoomb = true;
+
+
     void Start()
     {
-        test = GetComponent<Rigidbody>();
+        boomb.GetComponent<Rigidbody>().useGravity = false;
     }
     // Update is called once per frame
     void Update()
     {
-        //transform.Translate(Vector3.forward * speedBullet * Time.deltaTime);
-        test.AddForce(transform.forward* speedBullet);
-         
-
+        if(holdingBoomb)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                holdingBoomb = false;
+                boomb.GetComponent<Rigidbody>().useGravity = true;
+                boomb.GetComponent<Rigidbody>().AddForce(transform.forward*200f);
+            }
+        }
         timeLife -= Time.deltaTime;
         if (timeLife < 0)
         {
---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public Button btn_P;
+    public Button btn_E;
+
+
+    public void OnButtonPlayClick()
+    {
+        SceneManager.LoadScene(sceneName: "Game");
+    }
+
+    public void OnButtonExitClick()
+    {
+        Application.Quit();   
+    }
+}
