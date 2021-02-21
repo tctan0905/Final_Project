112306f7c6d082f57231845c37a1cc9cc31d6ff6
tctan0905 <tctan0905@users.noreply.github.com>
2021-02-21 16:02:17 +0700

    [TAN] Update code
    Add joystick

--- Assets/Scripts/BombController.cs
+++ Assets/Scripts/BombController.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class BombController : MonoBehaviour
 {
     public float speedBullet;
     public float timeLife;
-
+    public Rigidbody test;
+    void Start()
+    {
+        test = GetComponent<Rigidbody>();
+    }
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * speedBullet * Time.deltaTime);
+        //transform.Translate(Vector3.forward * speedBullet * Time.deltaTime);
+        test.AddForce(transform.forward* speedBullet);
+         
 
         timeLife -= Time.deltaTime;
         if (timeLife < 0)
