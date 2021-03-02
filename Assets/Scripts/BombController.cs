88f418c03a27c4016a18fabdca746013997511d0
tctan0905 <tctan0905@users.noreply.github.com>
2021-03-02 15:51:52 +0700

    [TAN] Update
    add effect bomb

--- Assets/Scripts/BombController.cs
+++ Assets/Scripts/BombController.cs
@@ -1,26 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 
 public class BombController : MonoBehaviour
 {
-    public float speedBullet;
-    public float timeLife;
-    public Rigidbody test;
 
-    public GameObject boomb;
-    public float bombDistance = 2f;
-    public float boombThrowingForce = 5f;
-
-    private bool holdingBoomb = true;
-    private int _count;
+    public GameObject explosionEffect;
+    public float delay = 3f;
+    public float explosionForece = 10f;
+    public float radius = 20f;
 
 
     void Start()
     {
-        //boomb.GetComponent<Rigidbody>().useGravity = true;
-        
+
+        Invoke("Explosion", delay);
     }
     // Update is called once per frame
     void Update()
@@ -28,6 +23,22 @@ public class BombController : MonoBehaviour
    
     }
 
+    private void Explosion()
+    {
+        Collider[] collider = Physics.OverlapSphere(transform.position, radius);
+
+        foreach(Collider near in collider)
+        {
+            Rigidbody rig = near.GetComponent<Rigidbody>();
+
+            if (rig != null)
+            {
+                rig.AddExplosionForce(explosionForece, transform.position, radius, 1, ForceMode.Impulse);
+            }
+        }
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
     private void OnEnable()
     {
         Debug.Log("Enable");
