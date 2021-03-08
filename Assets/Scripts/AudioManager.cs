c190c8c7036efb9c374524d99082a3eb4470c80e
trnhi1010 <57781716+trnhi1010@users.noreply.github.com>
2021-03-08 17:50:26 +0700

    [Nhi]: add audio
    -menu background audio
    -button effect

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource audioSource;
+    private float musicVolume = 1f;
+
+    private void Awake()
+    {
+        audioSource.Play();
+    }
+    private void Update()
+    {
+        audioSource.volume = musicVolume;
+    }
+
+    public void UpdateVolume(float volume)
+    {
+        musicVolume = volume;
+    }
+
+}
