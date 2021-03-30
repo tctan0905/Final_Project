61a805fdf14a422f6f6e603fa8cce1accedb58a9
hoangdat <hoangdat1994@gmail.com>
2021-03-30 18:59:33 +0700

    [DAT] update enemy animation

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -10,27 +10,52 @@ public class EnemyController : MonoBehaviour
     NavMeshAgent enemy;
     Transform target;
     Animator animator;
-    private float delayAttack;
-    private int timeBetween = 5;
+    private float distance;
+    private float _timeAttack = 3f;
+    private float _nextTimeAttack;
     void Awake()
     {
         target = PlayerManager.instance.player.transform;
         enemy = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
+        _nextTimeAttack = Time.time;
     }
     // Update is called once per frame
     void Update()
     {
+        distance = Vector3.Distance(target.position, transform.position);
         enemy.SetDestination(target.position);
-        delayAttack += Time.deltaTime;
-        if(delayAttack > timeBetween)
+        if(enemy.isStopped==false)
         {
-            Attack();
-            Debug.Log("Attack");
+            animator.SetBool("IsRun",true);
+        }else
+        {
+            animator.SetBool("IsRun",false);
         }
+        
+        if(distance <= enemy.stoppingDistance)
+         {
+             FaceTarget();
+             //Attack
+             Attack();
+         }
+
+        
     }
     void Attack()
     {
-        delayAttack = 0f;
+        if(_nextTimeAttack<Time.time)
+        {
+            _nextTimeAttack += _timeAttack;
+            animator.SetTrigger("TriggerAttack");
+        }
+
     }
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotaion = Quaternion.LookRotation(new Vector3(direction.x,0,direction.z));
+        transform.rotation =  Quaternion.Slerp(transform.rotation,lookRotaion,Time.deltaTime*5f);
+    }
+    
 }
